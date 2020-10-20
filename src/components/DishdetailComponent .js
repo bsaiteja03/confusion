67f5ce6ac39a17d5fe5d25e93769521e8cf463ad
bsaiteja03 <b.saiteja@klh.edu.in>
2020-10-20 23:02:07 +0530

    Combining Reducers

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -151,9 +151,8 @@ const DishDetailComponent = props => (
   <div className="container">
     <div className="row">
       <Breadcrumb>
-        <BreadcrumbItem>
-          <Link to="/menu">Menu</Link>
-        </BreadcrumbItem>
+      <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+      <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
         <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
       </Breadcrumb>
       <div className="col-12">
